8caa10ce476fa577c0dd38f1fcfdd582d9b1f0bd
LoreDoe <107372991+LoreDoe@users.noreply.github.com>
2025-02-11 23:18:12 -0300

    Update routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,6 +5,8 @@ const routes = [
     children: [
       { path: '/', name: 'home', component: () => import('pages/HomePage.vue') },
       { path: '/reservas', name: '/reservas', component: () => import('pages/ReservasPage.vue') },
+      { path: '/destinos', name: 'destinos', component: () => import('pages/DestinoPage.vue')},
+      { path: '/destinos/:id', name: 'destino-detalhes', component: () => import('pages/DestinoDetalhes.vue')},
     ]
   },
   { path: '/cad', name: 'cad', component: () => import('pages/CadCliente.vue') },
